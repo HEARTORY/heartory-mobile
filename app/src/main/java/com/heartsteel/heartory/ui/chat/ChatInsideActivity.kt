7884ef0f8482bab2f16d2ad189f4503e05154d4e
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2024-05-13 23:33:21 +0700

    [HuyNB] message lines inside chatbox

--- app/src/main/java/com/heartsteel/heartory/ui/chat/ChatInsideActivity.kt
+++ app/src/main/java/com/heartsteel/heartory/ui/chat/ChatInsideActivity.kt
@@ -12,7 +12,7 @@ class ChatInsideActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_chat_inside)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.chatInsideActivity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
